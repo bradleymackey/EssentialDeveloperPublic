fd98fe23fbd671ada19b7a16e20df3cd9a1f2df5
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:33 +0100

    EssentialFeed: format access control following conventions

--- projects/EssentialFeed/EssentialFeediOS/Controllers/FeedViewController.swift
+++ projects/EssentialFeed/EssentialFeediOS/Controllers/FeedViewController.swift
@@ -17,7 +17,7 @@ public protocol FeedImageDataLoader {
     func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
 }
 
-final public class FeedViewController: UITableViewController {
+public final class FeedViewController: UITableViewController {
     private var feedLoader: FeedLoader?
     private var imageLoader: FeedImageDataLoader?
     private var tableModel = [FeedImage]()
