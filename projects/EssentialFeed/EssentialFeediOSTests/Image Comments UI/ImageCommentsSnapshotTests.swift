203f52e4b6058434f1eb757172c999adc3718475
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:35 +0100

    EssentialFeed: Implement ImageComments UI

---
+++ projects/EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
@@ -0,0 +1,65 @@
+//
+//  ImageCommentsSnapshotTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Bradley Mackey on 26/03/2023.
+//
+
+import XCTest
+import EssentialFeediOS
+@testable import EssentialFeed
+
+class ImageCommentsSnapshotTests: XCTestCase {
+    
+    func test_listWithComments() {
+        let sut = makeSUT()
+        
+        sut.display(comments())
+        
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "IMAGE_COMMENTS_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "IMAGE_COMMENTS_dark")
+    }
+
+}
+
+// MARK: - Helpers
+
+extension ImageCommentsSnapshotTests {
+    
+    private func makeSUT() -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "ImageComments", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! ListViewController
+        controller.loadViewIfNeeded()
+        controller.tableView.showsVerticalScrollIndicator = false
+        controller.tableView.showsHorizontalScrollIndicator = false
+        return controller
+    }
+    
+    private func comments() -> [CellController] {
+        return [
+            ImageCommentCellController(
+                model: ImageCommentViewModel(
+                    message: "Several bridges named London Bridge have spanned the River Thames between the City of London and Southwark, in central London. The current crossing, which opened to traffic in 1973, is a box girder bridge built from concrete and steel. It replaced a 19th-century stone-arched bridge, which in turn superseded a 600-year-old stone-built medieval structure.",
+                    date: "12345 years ago",
+                    username: "long long long long long long long long long long"
+                )
+            ),
+            ImageCommentCellController(
+                model: ImageCommentViewModel(
+                    message: "A\nmessage\nwith\nmultiple\nlines",
+                    date: "77 weeks ago",
+                    username: "username hh"
+                )
+            ),
+            ImageCommentCellController(
+                model: ImageCommentViewModel(
+                    message: "nice",
+                    date: "just now",
+                    username: "l"
+                )
+            ),
+        ]
+    }
+    
+}
