af136fbf9a53740844619fe91fea557855f763fd
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:33 +0100

    EssentialFeed: extract custom assertions to seperate file

--- projects/EssentialFeed/EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
+++ projects/EssentialFeed/EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
@@ -178,31 +178,6 @@ final class FeedViewControllerTests: XCTestCase {
 
 extension FeedViewControllerTests {
     
-    private func assertThat(_ sut: FeedViewController, isRendering imageModels: [FeedImage], file: StaticString = #file, line: UInt = #line) {
-        let actualRendered = sut.numberOfRenderedFeedImageViews()
-        guard actualRendered == imageModels.count else {
-            return XCTFail("Expected to render \(imageModels.count), but rendered \(actualRendered) instead!", file: file, line: line)
-        }
-        
-        for (index, imageModel) in imageModels.enumerated() {
-            assertThat(sut, hasViewConfiguredFor: imageModel, at: index, file: file, line: line)
-        }
-    }
-    
-    private func assertThat(_ sut: FeedViewController, hasViewConfiguredFor image: FeedImage, at index: Int, file: StaticString = #file, line: UInt = #line) {
-        let view = sut.feedImageView(at: index)
-        guard let cell = view as? FeedImageCell else {
-            return XCTFail("Expected \(FeedImageCell.self) instance, got \(String(describing: view)) instead!", file: file, line: line)
-        }
-        
-        let shouldLocationBeVisible = (image.location != nil)
-        XCTAssertEqual(cell.isShowingLocation, shouldLocationBeVisible, "Expected 'isShowingLocation' to be \(shouldLocationBeVisible) for image view at index \(index)", file: file, line: line)
-        
-        XCTAssertEqual(cell.locationText, image.location, "Expected location text to be \(String(describing: image.location)) for image view at index \(index)", file: file, line: line)
-        
-        XCTAssertEqual(cell.descriptionText, image.description, "Expected description text to be \(String(describing: image.description)) for image view at index \(index)", file: file, line: line)
-    }
-    
     func test_feedImageViewRetryButton_isVisibleOnImageURLLoadError() {
         let (sut, loader) = makeSUT()
         
