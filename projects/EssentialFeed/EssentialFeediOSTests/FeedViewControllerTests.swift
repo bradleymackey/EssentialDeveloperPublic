77a0af9c914b1dfa81f96901378de173dbbb730c
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:33 +0100

    EssentialFeed: Load feed on viewDidLoad

--- projects/EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ projects/EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -8,9 +8,20 @@
 import XCTest
 import UIKit
 
-final class FeedViewController {
-    init(loader: FeedViewControllerTests.LoaderSpy) {
+final class FeedViewController: UIViewController {
+    private var loader: FeedViewControllerTests.LoaderSpy?
+    
+    // We can use a convenience init since we don't need any custom initialization.
+    // This way, we don't need to implement the view controller's required initializers!
+    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        loader?.load()
     }
 }
 
@@ -23,6 +34,18 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 0)
     }
     
+    func test_viewDidLoad_loadsFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        // Knowledge of 3rd party framework is needed.
+        // We need to know what UIKit does when this method is called, so we can write our test correctly!
+        // We shouldn't be testing the framework, just the implementation of our own code, we assume UIKit works perfectly.
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+    
 }
 
 // MARK: - Helpers
@@ -31,6 +54,10 @@ extension FeedViewControllerTests {
     
     final class LoaderSpy {
         private(set) var loadCallCount = 0
+        
+        func load() {
+            loadCallCount += 1
+        }
     }
     
 }
