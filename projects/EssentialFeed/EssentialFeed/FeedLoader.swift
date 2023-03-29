b92038a2f976f5d2e15fe9aaaa1abd117add682a
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:32 +0100

    EssentialFeed: seperate file for RemoteFeedLoader

--- projects/EssentialFeed/EssentialFeed/FeedLoader.swift
+++ projects/EssentialFeed/EssentialFeed/FeedLoader.swift
@@ -10,19 +10,3 @@ import Foundation
 protocol FeedLoader {
     func load(completion: @escaping (Result<[FeedItem], Error>) -> Void)
 }
-
-// MARK: - Remote
-
-class RemoteFeedLoader {
-    // Inject the HTTP client dependency to ensure there is not a strong coupling
-    let client: HTTPClient
-    let url: URL
-    init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    func load() {
-        client.get(from: url)
-    }
-}
-
--- projects/EssentialFeed/EssentialFeed/RemoteFeedLoader.swift
+++ projects/EssentialFeed/EssentialFeed/RemoteFeedLoader.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class RemoteFeedLoader {
+    // Inject the HTTP client dependency to ensure there is not a strong coupling
+    let client: HTTPClient
+    let url: URL
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    func load() {
+        client.get(from: url)
+    }
+}
