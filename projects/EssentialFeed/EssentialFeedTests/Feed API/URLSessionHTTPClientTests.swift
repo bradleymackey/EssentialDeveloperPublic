a7082881ac611113ac495fc7db2e40ebed050ea8
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:32 +0100

    EssentialFeed: delivers failure with error on request error

--- projects/EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ projects/EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class URLSessionHTTPClient {
     private let session: URLSession
@@ -14,8 +15,12 @@ class URLSessionHTTPClient {
         self.session = session
     }
     
-    func get(from url: URL) {
-        session.dataTask(with: url) { _, _, _ in }.resume()
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { _, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+        }.resume()
     }
 }
 
@@ -29,11 +34,37 @@ class URLSessionHTTPClientTests: XCTestCase {
         
         let sut = URLSessionHTTPClient(session: session)
         
-        sut.get(from: url)
+        sut.get(from: url) { _ in }
         
         // we don't really want to test the internal 'resume' behaviour of URLSession
         // this is an implementation detail, but this test does work
         XCTAssertEqual(task.resumeCallCount, 1)
+    }
+    
+    func test_getFromURL_failsOnRequestError() {
+        let url = URL(string: "https://any-url.com")!
+        let expectedError = NSError(domain: "any error", code: 1)
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        
+        session.stub(url: url, error: expectedError)
+        
+        let sut = URLSessionHTTPClient(session: session)
+        
+        let exp = expectation(description: "Wait for completion block")
+        sut.get(from: url) { result in
+            switch result {
+            case let .failure(error as NSError):
+                XCTAssertEqual(error, expectedError)
+            default:
+                XCTFail("Expected failure with \(expectedError), got \(result) instead")
+            }
+            
+            exp.fulfill()
+            
+        }
+        
+        wait(for: [exp], timeout: 1)
     }
     
     // MARK: - Helpers
@@ -41,14 +72,23 @@ class URLSessionHTTPClientTests: XCTestCase {
     // Overriding = bunch of assumptions, methods that we are not overriding!!
     // This is very fragile!!!
     private class URLSessionSpy: URLSession {
-        private var stubs = [URL: URLSessionDataTask]()
+        private var stubs = [URL: Stub]()
         
-        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-            return stubs[url] ?? FakeURLSessionDataTask()
+        private struct Stub {
+            let task: URLSessionDataTask
+            let error: Error?
         }
         
-        func stub(url: URL, task: URLSessionDataTask) {
-            stubs[url] = task
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            guard let stub = stubs[url] else {
+                fatalError("Could not find stub for \(url)")
+            }
+            completionHandler(nil, nil, stub.error)
+            return stub.task
+        }
+        
+        func stub(url: URL, task: URLSessionDataTask = FakeURLSessionDataTask(), error: Error? = nil) {
+            stubs[url] = Stub(task: task, error: error)
         }
     }
     
