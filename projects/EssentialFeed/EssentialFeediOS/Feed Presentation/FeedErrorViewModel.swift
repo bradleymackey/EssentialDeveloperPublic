7ae6bdbb00101c5ba8cd2d4aa45e6de9c645820e
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:33 +0100

    EssentialFeed: Add FeedErrorViewModel factory helpers to clarify intent

--- projects/EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
+++ projects/EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -11,3 +11,13 @@ struct FeedErrorViewModel {
     let message: String?
 }
 
+extension FeedErrorViewModel {
+    static var noError: FeedErrorViewModel {
+        FeedErrorViewModel(message: nil)
+    }
+    
+    static func error(message: String) -> FeedErrorViewModel {
+        FeedErrorViewModel(message: message)
+    }
+}
+
