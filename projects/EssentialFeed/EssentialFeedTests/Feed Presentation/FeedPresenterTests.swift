e5482aff051f3cde0183388bfcfd96176ebc4f98
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:33 +0100

    EssentialFeed: extract system under test creation to a test factory method

--- projects/EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ projects/EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -29,6 +29,14 @@ class FeedPresenterTests: XCTestCase {
 
 extension FeedPresenterTests {
     
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedPresenter(view: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
+    }
+    
     private class ViewSpy {
         let messages = [Any]()
     }
