8220fba6b88841ce1d956faff2bfc45fe8649991
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:32 +0100

    EssentialFeed: track for memory leaks when creating SUT

--- projects/EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
+++ projects/EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
@@ -50,9 +50,11 @@ final class CacheFeedUseCaseTests: XCTestCase {
 
 extension CacheFeedUseCaseTests {
     
-    func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+    func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
         let store = FeedStore()
         let sut = LocalFeedLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
     }
     
