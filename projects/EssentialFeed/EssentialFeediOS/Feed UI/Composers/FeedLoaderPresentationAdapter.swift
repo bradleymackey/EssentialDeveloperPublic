11422cbb57fde17fba1b220be3da0918aa70b204
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:33 +0100

    EssentialFeed: more composer components to seperate files

---
+++ projects/EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,35 @@
+//
+//  FeedLoaderPresentationAdapter.swift
+//  EssentialFeediOS
+//
+//  Created by Bradley Mackey on 21/02/2023.
+//
+
+import Foundation
+import EssentialFeed
+
+final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
+    private let feedLoader: FeedLoader
+    /// This is only optional to break a circular dependency.
+    /// It's fine though, because this is at the composition layer.
+    /// We're not leaking this composition detail into the adapters.
+    ///
+    /// (Constructor injection should be preferred whereever possible though!)
+    var presenter: FeedPresenter?
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    func didRequestFeedRefresh() {
+        presenter?.didStartLoadingFeed()
+        feedLoader.load { [weak self] result in
+            switch result {
+            case .success(let feed):
+                self?.presenter?.didFinishLoadingFeed(with: feed)
+            case .failure(let error):
+                self?.presenter?.didFinishLoadingFeed(with: error)
+            }
+        }
+    }
+}
