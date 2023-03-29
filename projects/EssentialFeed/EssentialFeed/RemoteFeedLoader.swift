f365f78cff2266050b62c2f8ac803a3ee9ef06da
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:32 +0100

    EssentialFeed: add notes about single responsibility of the FeedItemsMapper

--- projects/EssentialFeed/EssentialFeed/RemoteFeedLoader.swift
+++ projects/EssentialFeed/EssentialFeed/RemoteFeedLoader.swift
@@ -70,6 +70,16 @@ private class FeedItemsMapper {
     }
     
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        // We are decoding AND checking the status code here.
+        // However, this is not a violation of the single responsibility principle because
+        // this functionality is based on the API contract, which includes the status code.
+        //
+        // If the status code rule changes, it's a breaking change in the API contract and
+        // we need to update the component to reflect the changes.
+        //
+        // If, for example, there was a 401 repsonse this component would simply reject the response (rather than handling reauthentication).
+        // Another component would handle the authentication so that this component could
+        // then fetch valid data from a 200 response.
         guard response.statusCode == 200 else {
             throw RemoteFeedLoader.Error.invalidData
         }
