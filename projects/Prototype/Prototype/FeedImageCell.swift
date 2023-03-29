85f2c6f6f2ae169765a5a0e89ecd7966fdce54ac
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:33 +0100

    Prototype: add FeedImageCell with storyboard connections

---
+++ projects/Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by Bradley Mackey on 07/08/2022.
+//
+
+import UIKit
+
+final class FeedImageCell: UITableViewCell {
+    @IBOutlet private(set) var locationContainer: UIView!
+    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var feedImageView: UIImageView!
+    @IBOutlet private(set) var descriptionLabel: UILabel!
+}
