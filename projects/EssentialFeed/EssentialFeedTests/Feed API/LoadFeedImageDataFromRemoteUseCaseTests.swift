f753b9de0cd0689227c18c0844ba72f3ffab3295
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:34 +0100

    EssentialFeed: Rename test case class to reflect the use case it refers to

--- projects/EssentialFeed/EssentialFeedTests/Feed API/LoadFeedImageDataFromRemoteUseCaseTests.swift
+++ projects/EssentialFeed/EssentialFeedTests/Feed API/LoadFeedImageDataFromRemoteUseCaseTests.swift
@@ -1,5 +1,5 @@
 //
-//  RemoteFeedImageDataLoaderTests.swift
+//  LoadFeedImageDataFromRemoteUseCaseTests.swift
 //  EssentialFeedTests
 //
 //  Created by Bradley Mackey on 02/03/2023.
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-final class RemoteFeedImageDataLoaderTests: XCTestCase {
+final class LoadFeedImageDataFromRemoteUseCaseTests: XCTestCase {
     func test_init_doesNotPerformAnyURLRequest() {
         let (_, client) = makeSUT()
         
@@ -113,7 +113,7 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
     }
 }
 
-extension RemoteFeedImageDataLoaderTests {
+extension LoadFeedImageDataFromRemoteUseCaseTests {
     private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedImageDataLoader(client: client)
