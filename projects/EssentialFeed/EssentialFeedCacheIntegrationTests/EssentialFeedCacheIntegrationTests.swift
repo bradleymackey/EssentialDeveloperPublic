58c8158403aeb01b071d2ab67931e52371505f58
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:32 +0100

    EssentialFeed: add EssentialFeedCacheIntegrationTests, a target for potentially slower integration tests

---
+++ projects/EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Bradley Mackey on 28/05/2022.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
