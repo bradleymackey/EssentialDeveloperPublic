4e26436fd423f924502e2075b6b6a393432b6494
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:33 +0100

    EssentialFeed: move FeedErrorViewModel to seperate file

---
+++ projects/EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Bradley Mackey on 01/03/2023.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
+
