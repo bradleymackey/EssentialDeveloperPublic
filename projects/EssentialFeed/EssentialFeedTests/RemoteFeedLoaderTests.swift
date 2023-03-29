0394faef0502ef3d1d96d1bc090bd496ff99e65a
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:32 +0100

    EssentialFeed: add sut test helper maker method

--- projects/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ projects/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -22,17 +22,14 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
     
     func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "https://someurl.com")!
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(url: url, client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
     func test_load_requestDataFromURL() {
         let url = URL(string: "https://someurl.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         sut.load { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url])
@@ -40,8 +37,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_loadTwice_requestDataFromURL() {
         let url = URL(string: "https://someurl.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         sut.load { _ in }
         sut.load { _ in }
         
@@ -50,9 +46,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnClientError() {
         // ARRANGE
-        let url = URL(string: "https://someurl.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT()
         
         // ACT
         // (client and completions belongs to the 'act')
@@ -74,9 +68,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
     
     func test_load_deliversErrorOnNon200Response() {
-        let url = URL(string: "https://someurl.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT()
         
         // Having a range of samples is usually good enough.
         // This tests a variety of condiditions without going overboard.
@@ -92,9 +84,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOn200ResponseWithInvalidJSON() {
         // ARRANGE
-        let url = URL(string: "https://someurl.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT()
         
         // ACT, ASSERT
         expect(sut, toCompleteWith: .failure(.invalidData)) {
@@ -105,9 +95,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversNoItemsOn200ResponseWithEmptyJSONList() {
         // ARRANGE
-        let url = URL(string: "https://someurl.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT()
         
         // ACT, ASSERT
         expect(sut, toCompleteWith: .success([])) {
@@ -118,9 +106,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversItemsOn200HTTPResponseWithJSONItems() {
         // ARRANGE
-        let url = URL(string: "https://someurl.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT()
         
         let item1 = makeItem(
             id: UUID(),
@@ -189,6 +175,13 @@ extension RemoteFeedLoaderTests {
             )!
             messages[index].completion(.success(data, response))
         }
+    }
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        
+        return (sut, client)
     }
     
     private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedItem, json: [String: Any]) {
