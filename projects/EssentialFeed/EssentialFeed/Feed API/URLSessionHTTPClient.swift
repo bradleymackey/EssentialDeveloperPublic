25636ef9908f5df4756396ed37911b6f31fe6363
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:32 +0100

    EssentialFeed: move URLSessionHTTPClient to it's own file in production.

---
+++ projects/EssentialFeed/EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -0,0 +1,34 @@
+//
+//  URLSessionHTTPClient.swift
+//  EssentialFeed
+//
+//  Created by Bradley Mackey on 14/04/2022.
+//
+
+import Foundation
+
+// NOTE: this could just be an extension on URLSession
+// but extending types that you don't own could lead to namespace conflicts in the
+// future with function names etc., so having our own adapter type gives us a bit more control
+
+public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    private struct InvalidRepresentationError: Error {}
+    
+    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(InvalidRepresentationError()))
+            }
+        }.resume()
+    }
+}
