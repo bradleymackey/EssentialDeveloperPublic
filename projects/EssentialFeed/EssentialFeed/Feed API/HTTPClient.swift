e82db305e7b3cbae1ab6a453a6e3946ccd7f7c53
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:32 +0100

    EssentialFeed: document that HTTPClient completion handler can be invoked in any thread

--- projects/EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
+++ projects/EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
@@ -10,6 +10,8 @@ import Foundation
 // There's no reason that HTTPClient needs to be a singleton.
 // We can extend URLSession
 public protocol HTTPClient {
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible for dispatch to the appropriate threads, if needed.
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
