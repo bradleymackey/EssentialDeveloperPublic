bd408cbeaad72646fe08988b7e134907c9469da7
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:35 +0100

    EssentialFeed: render load more error message

--- projects/EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ projects/EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -120,6 +120,10 @@ extension ListViewController {
         loadMoreFeedCell()?.isLoading == true
     }
     
+    var loadMoreFeedErrorMessage: String? {
+        loadMoreFeedCell()?.message
+    }
+    
     private func loadMoreFeedCell() -> LoadMoreCell? {
         cell(row: 0, section: feedLoadMoreSection) as? LoadMoreCell
     }
