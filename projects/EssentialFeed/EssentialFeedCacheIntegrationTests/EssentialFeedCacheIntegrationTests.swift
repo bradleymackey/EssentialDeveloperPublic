c91f61f0223907cf4a7383ac734e14addd5acf6c
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:33 +0100

    EssentialFeed: fix cache integration tests for new result type from save

--- projects/EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ projects/EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -69,9 +69,15 @@ extension EssentialFeedCacheIntegrationTests {
     
     private func save(_ feed: [FeedImage], with loader: LocalFeedLoader, file: StaticString = #file, line: UInt = #line) {
         let saveExp = expectation(description: "Wait for save completion")
-        loader.save(feed) { saveError in
-            XCTAssertNil(saveError, "Expected to save feed successfully", file: file, line: line)
-            saveExp.fulfill()
+        loader.save(feed) { result in
+            defer { saveExp.fulfill() }
+            
+            switch result {
+            case .success:
+                break
+            case .failure:
+                XCTFail("Expected to save feed successfully", file: file, line: line)
+            }
         }
         wait(for: [saveExp], timeout: 1.0)
     }
@@ -79,6 +85,8 @@ extension EssentialFeedCacheIntegrationTests {
     private func expect(_ sut: LocalFeedLoader, toLoad expectedFeed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Wait for load completion")
         sut.load { result in
+            defer { exp.fulfill() }
+            
             switch result {
             case let .success(loadedFeed):
                 XCTAssertEqual(loadedFeed, expectedFeed, file: file, line: line)
@@ -86,8 +94,6 @@ extension EssentialFeedCacheIntegrationTests {
             case let .failure(error):
                 XCTFail("Expected successful feed result, got \(error) instead", file: file, line: line)
             }
-            
-            exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
     }
