c9cdca93361e213821c0d412e643b5cd1fd57da2
Bradley Mackey <bradleymackey@me.com>
2023-03-29 09:57:32 +0100

    EssentialFeed: does not delete cache on localfeedloader init

---
+++ projects/EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
@@ -0,0 +1,29 @@
+//
+//  CacheFeedUseCase.swift
+//  EssentialFeedTests
+//
+//  Created by Bradley Mackey on 28/04/2022.
+//
+
+import XCTest
+
+class LocalFeedLoader {
+    init(store: FeedStore) {
+        
+    }
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
+final class CacheFeedUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+}
